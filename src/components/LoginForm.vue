65b2d3ecf6ddfd19593029f4e81368ed4b79284b
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-10-18 18:07:05 +0500

    feat: add LoginForm component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <form class="login-form">
+        <div class="inputs-container">
+            <UiInput placeholder="E-mail" input-number="01">
+                <template #icon>
+                    <UiIcon icon="question" />
+                </template>
+            </UiInput>
+
+            <UiInput placeholder="Пароль" type="password" input-number="02">
+                <template #icon>
+                    <UiIcon icon="password-toggle" />
+                </template>
+            </UiInput>
+        </div>
+        <div class="forget-password">
+            <a class="forget-password__link">{{ FORGET_PASSWORD_TEXT }}</a>
+        </div>
+        <div class="button-container">
+            <UiButton type="button">Войти</UiButton>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import UiInput from './UI/UiInput.vue';
+import UiIcon from './UI/UiIcon.vue';
+import UiButton from './UI/UiButton.vue';
+
+const FORGET_PASSWORD_TEXT = 'Забыли пароль?';
+</script>
+
+<style scoped>
+.inputs-container {
+    & > * {
+        border-top: 2px solid var(--primary);
+
+        &:last-child {
+            border-bottom: 2px solid var(--primary);
+        }
+    }
+}
+
+.forget-password {
+    position: relative;
+    margin: 24px 0 32px 0;
+    font-family: 'Germano';
+    font-size: 64px;
+    color: var(--primary);
+    text-align: center;
+}
+
+.forget-password__link {
+    position: relative;
+    left: -250px;
+}
+
+.button-container {
+    display: flex;
+    justify-content: center;
+    padding-top: 100px;
+}
+
+.agreement {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,12 +1,14 @@
 <template>
     <AuthHeadlines v-model="currentView" />
-    <RegistryForm />
+    <RegistryForm v-if="currentView === 'registry'" />
+    <LoginForm v-else-if="currentView === 'login'" />
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import AuthHeadlines from '../components/AuthHeadlines.vue';
 import RegistryForm from '../components/RegistryForm.vue';
+import LoginForm from '../components/LoginForm.vue';
 
 const currentView = ref('registry');
 </script>
