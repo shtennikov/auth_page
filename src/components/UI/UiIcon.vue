986f5dc16c233a2e3068f506ed5d3ed5cda054cc
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-10-18 14:21:55 +0500

    feat: add UiIcon component

---
+++ src/components/UI/UiIcon.vue
@@ -0,0 +1,32 @@
+<template>
+    <img class="icon" :src="iconSrc" :alt="icon" />
+</template>
+
+<script>
+import { defineProps, computed } from 'vue';
+import PasswordToggle from '../../assets/icons/password-toggle.svg';
+import PasswordToggleAlert from '../../assets/icons/password-toggle-alert.svg';
+import Question from '../../assets/icons/question.svg';
+import QuestionAlert from '../../assets/icons/question-alert.svg';
+
+const icons = {
+    'password-toggle': PasswordToggle,
+    'password-toggle-alert': PasswordToggleAlert,
+    question: Question,
+    'question-alert': QuestionAlert
+};
+</script>
+
+<script setup>
+const props = defineProps({
+    icon: {
+        type: String,
+        required: true,
+        validator: (value) => Object.keys(icons).includes(value)
+    }
+});
+
+const iconSrc = computed(() => icons[props.icon]);
+</script>
+
+<style scoped></style>
